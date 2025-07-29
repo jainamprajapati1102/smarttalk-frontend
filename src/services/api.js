ab0876cae306fd0b897e5778d4e36dcb80cb8f79
jainamprajapati1102 <jainamprajapati1102@gmail.com>
2025-07-29 23:09:39 +0530

    change in uri

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 // services/api.js
 import axios from "axios";
 
-const  api = axios.create({
-  baseURL: "http://localhost:5100", // Your backend base URL
-    headers: {
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL, // Your backend base URL
+  headers: {
     "Content-Type": "application/json",
   },
   withCredentials: true, // Only if backend uses cookies/sessions
